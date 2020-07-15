e76fec690cdc46ac96de7fba3c92f531e22043e7
Gregory Reeves Jr <gregoryreevesjr@gmail.com>
2020-07-14 22:54:10 -0500

    Assignment  completed and updated

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var reversed = "";
+            
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word[i];
+                reversed.Reverse();
+         
+            }
+
+            if (reversed.ToString() == word)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
